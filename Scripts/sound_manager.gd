5b4de7b2524779a6d1a0d973f51a9af90630f21f
drope <bartolomeip16@gmail.com>
2025-08-24 10:30:09 +0200

    testing

--- Scripts/sound_manager.gd
+++ Scripts/sound_manager.gd
@@ -4,6 +4,7 @@ extends Node
 
 static var children_array = []
 
+# ceci est un commit
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for lChilds_numb: AudioStreamPlayer in get_children():
