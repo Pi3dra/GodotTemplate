2f2681411d0c7b5246bcea3972d82ddabcfdab3f
Hec <150187921+Rotceh999@users.noreply.github.com>
2025-08-30 16:19:50 +0200

    cursor

---
+++ Scripts/Tools/mouse_cursor.gd
@@ -0,0 +1,12 @@
+extends TextureRect
+
+class_name Cursor
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position = get_viewport().get_mouse_position()
