caa3f857c7f191d5516a7eef1ebd53ba806a95bd
Hec <150187921+Rotceh999@users.noreply.github.com>
2025-09-11 15:47:53 +0200

    cookie panel info spawn

---
+++ Scripts/UI/cookie_info.gd
@@ -0,0 +1,27 @@
+extends Control
+
+@onready var texture_panel = $VBoxContainer/PanelContainer/TextureRect
+@onready var description_box = $VBoxContainer/Description
+@onready var cookie_title = $Panel/CookieName
+
+var cookie_type : Cookie.TYPE
+var cookie_price : int
+
+func _ready():
+	pass
+
+	
+	
+func update_panel(cookie : Cookie.TYPE, locked : bool  = false):
+	cookie_type = cookie
+	var cookie_data = Globals.get_cookie_data(cookie_type)
+	cookie_price = cookie_data.price
+	
+	if locked:
+		texture_panel.texture = null
+		description_box.text = "Locked"
+		cookie_title.text = "Locked"
+	else:
+		texture_panel.texture = cookie_data.head_texture
+		description_box.text = cookie_data.description
+		cookie_title.text = Cookie.TYPE.keys()[cookie] + " Cookie"
